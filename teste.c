81be721c126f3b932cad81d48a517006a5933254
rogerpeter <lobowolf1@gmail.com>
2017-07-16 15:20:19 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -3,14 +3,16 @@
 #include "config.h"
 #include "comum.h"
 
-
-
 int main(){
   TAux fila = NULL;
+  
+  ler_Num(&fila);
+  /*
   inserir(&fila, 2, PILHA, DESORDENADO);
   inserir(&fila, 3, PILHA, DESORDENADO);
   inserir(&fila, 4, PILHA, DESORDENADO);
   inserir(&fila, 5, PILHA, DESORDENADO);
   inserir(&fila, 6, PILHA, DESORDENADO);
+  /* */
   exibir(fila);
 }
