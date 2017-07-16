fdaaff69a10a87823ea8d7f94114f35ee924eeca
rogerpeter <lobowolf1@gmail.com>
2017-07-16 19:43:01 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -4,30 +4,30 @@
 #include "comum.h"
 
 int main(){
-  TAux lista1 = NULL, lista2 = NULL;
-  int info;
+  	TAux lista1 = NULL, lista2 = NULL;
+  	int info;
   
   
-  ler_Num(&lista2, LISTA, CRESCENTE);
-  exibir(lista2);
-  /* */
+  	ler_Num(&lista2, LISTA, CRESCENTE);
+  	exibir(lista2);
+  	/* */
   
   
-  inserir(&lista1, 2, LISTA, DECRESCENTE);
-  inserir(&lista1, 3, LISTA, DECRESCENTE);
-  inserir(&lista1, 4, LISTA, DECRESCENTE);
-  inserir(&lista1, 5, LISTA, DECRESCENTE);
-  inserir(&lista1, 6, LISTA, DECRESCENTE);
+  	inserir(&lista1, 2, LISTA, DECRESCENTE);
+  	inserir(&lista1, 3, LISTA, DECRESCENTE);
+  	inserir(&lista1, 4, LISTA, DECRESCENTE);
+	inserir(&lista1, 5, LISTA, DECRESCENTE);
+  	inserir(&lista1, 6, LISTA, DECRESCENTE);
   
-  exibir(lista1);
-  /* */
+  	exibir(lista1);
+  	/* */
   
   
-  info = comparar(lista1,lista2);
+  	info = comparar(lista1,lista2);
   
-  if(info==0)
-	printf("\n DIFERENTES \n");
-  else
-  	printf("\n IGUAIS \n");
-  /* */
+  	if(info==0)
+		printf("\n DIFERENTES \n");
+  	else
+  		printf("\n IGUAIS \n");
+  	/* */
 }
